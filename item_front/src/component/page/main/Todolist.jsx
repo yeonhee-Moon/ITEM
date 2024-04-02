5a78591ef8dcf0ba78abd332d82ca4dc41e09b57
unknown <xsss7@naver.com>
2024-04-02 16:58:11 +0900

    수정

--- item_front/src/component/page/main/Todolist.jsx
+++ item_front/src/component/page/main/Todolist.jsx
@@ -1,54 +1,19 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import CompleteButton from "../ui/CompleteButton";
 import ConfirmButton from "../ui/ConfirmButton";
 import TutorButton from "../ui/TutorButton";
 import DeleteButton from "../ui/DeleteButton"
+import UpdateInput from "../ui/UpdateInput"
+
 
 function TodoList({ todos, updateTodo, onTodoClick, onComplete, updateId, onConfirm, onLined, onDelete}) {
-  const inputRef = useRef(null);
-  const [editedTodo, setEditedTodo] = useState({ id: null, title: '' });
-  
-  useEffect(() => {
-    // 외부 클릭 시 이벤트 처리
-    const handleClickOutside = (event) => {
-      if (inputRef.current && !inputRef.current.contains(event.target)) {
-        // 입력 필드 외부를 클릭한 경우에만 처리
-        updateTodo({
-          id: editedTodo.id,
-          title: editedTodo.title
-        });
-        setEditedTodo({ id: null, title: '' });
-      }
-    };
-
-    document.addEventListener('mousedown', handleClickOutside);
-
-    // 컴포넌트가 언마운트될 때 이벤트 리스너 제거
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, [editedTodo, updateTodo]);
-
-  const handleChange = (event, todo) => {
-    setEditedTodo({
-      // ...editedTodo,
-      id : todo.id,
-      title: event.target.value
-    });
-  };
-
 
   return (
     <ul>
       {todos.map((todo) => (
         <li key={todo.id}>
           {updateId === todo.id ? (
-            <input
-              type="text"
-              ref={inputRef}
-              value={editedTodo.id === todo.id ? editedTodo.title : todo.title}
-              onChange={(event) => handleChange(event, todo)}
-            />
+            <UpdateInput todo={todo} updateTodo={updateTodo}/>
           ) : (
             todo.lined ? (<s>{todo.title}</s>) : (<span onClick={() => onTodoClick(todo.id)}>{todo.title}</span>)
           )}
---
+++ item_front/src/component/page/ui/UpdateInput.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+function UpdateInput({todo, updateTodo}) {
+    const inputRef = useRef(null);
+    const [editedTodo, setEditedTodo] = useState({ id: null, title: '' });
+  
+    useEffect(() => {
+      // 외부 클릭 시 이벤트 처리
+      const handleClickOutside = (event) => {
+        if (inputRef.current && !inputRef.current.contains(event.target)) {
+          // 입력 필드 외부를 클릭한 경우에만 처리
+          if (editedTodo.id !== null) {
+          updateTodo({
+            id: editedTodo.id,
+            title: editedTodo.title
+          });
+          setEditedTodo({ id: null, title: '' });
+          } else {
+            updateTodo({
+            id: editedTodo.id,
+            title: todo.title
+          });
+          setEditedTodo({ id: null, title: '' });}
+        }
+      };
+  
+      document.addEventListener('mousedown', handleClickOutside);
+  
+      // 컴포넌트가 언마운트될 때 이벤트 리스너 제거
+      return () => {
+        document.removeEventListener('mousedown', handleClickOutside);
+      };
+    }, [editedTodo, updateTodo]);
+  
+    const handleChange = (event, todo) => {
+      setEditedTodo({
+        // ...editedTodo,
+        id : todo.id,
+        title: event.target.value
+      });
+  
+      
+    };
+  // console.log('title : ',exceptTodo);
+  
+    return (
+        <input
+        type="text"
+        ref={inputRef}
+        value={editedTodo.id === todo.id ? editedTodo.title : todo.title}
+        onChange={(event) => handleChange(event, todo)}
+        />
+    );
+}
+    
+export default UpdateInput;
